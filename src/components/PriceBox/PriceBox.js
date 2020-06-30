85bdc829a14094f99f9535ecfa4a26593e0575cd
Robin Steeman <Robin_steeman@hotmail.com>
2020-06-30 10:55:50 +0200

    Added color changes on positive or negative % and minor changes PriceBox

--- src/components/PriceBox/PriceBox.js
+++ src/components/PriceBox/PriceBox.js
@@ -8,7 +8,11 @@ const PriceBox = ({ ticker }) => {
    // Get price and convert to right format
    let currentData = useGetData(`price?symbol=${ticker}`);
    let currentPrice = currentData.price;
-   currentPrice = currentPrice ? currentPrice.slice(0, -6) : '';
+   if (ticker.includes('USDT')) {
+      currentPrice = currentPrice ? currentPrice.slice(0, -6) : '';
+   } else {
+      currentPrice = currentPrice ? currentPrice.split('.')[1] - 0 : '';
+   }
 
    // Get 24H change percentage
    let x = useGetData(`24hr?symbol=${ticker}`);
@@ -20,9 +24,15 @@ const PriceBox = ({ ticker }) => {
          <div className='ticker'>
             {ticker.substr(0, 3) + '/' + ticker.substr(3)}
             <div>
-               <div className='price negative'>
+               <div className='price positive'>
                   {currentPrice}
-                  <span className='percent negative'>{`${currentPercent}%`}</span>
+                  <span
+                     className={
+                        currentPercent < 0
+                           ? 'percent negative'
+                           : 'percent positive'
+                     }
+                  >{`${currentPercent}%`}</span>
                </div>
             </div>
          </div>
